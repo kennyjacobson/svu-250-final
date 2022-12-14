fbd3122779b77daaf86a57d493474334721249cc
kennyjacobson <kenny.jacobson@gmail.com>
2022-12-14 12:29:58 -0800

    restoring test file

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -22,10 +22,7 @@ class test_greet(unittest.TestCase):
         """
         #ASSIGNMENT: you will write the code to test the GIVEN/WHEN/THEN above.
 
-        name = ""
-        expected_result = "Hello, stranger."
-        result = greet(name)
-        #self.assertEqual(result, expected_result) #comment this out after you write the test.
+        self.assertEqual(0, 1) #comment this out after you write the test.
 
 class test_get_average(unittest.TestCase):
     def test_get_average(self):
@@ -34,13 +31,9 @@ class test_get_average(unittest.TestCase):
         WHEN the greet get_average is called with the parameters 2 and 4
         THEN the function should return the float 3.0
         """
-        #ASSIGNMENT: you will write the code to test the GIVEN/WHEN/THEN above
-        num1 = 2
-        num2 = 4
-        expected_result = 3.0
-        result = get_average(2, 4)
-        self.assertEqual(result, expected_result)
-        #self.assertEqual(0, 1) #comment this out after you write the test.
+        #ASSIGNMENT: you will write the code to test the GIVEN/WHEN/THEN above.
+        
+        self.assertEqual(0, 1) #comment this out after you write the test.
 
 
         
